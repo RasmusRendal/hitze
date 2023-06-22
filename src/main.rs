5b6bf1d6fb2f82f9da9f6d78a3578c571ddc5c44
Rasmus Rendal <rasmus@rend.al>
2023-06-22 17:00:31 +0200

    Add hacky looping to fix golf example

--- src/main.rs
+++ src/main.rs
@@ -105,52 +105,59 @@ fn parse(code : &String) -> Vec<Instruction> {
             _ => {}
         }
     }
+    for instr in output.iter() {
+        match instr {
+            Instruction::LoopBegin(i) => {
+                assert!(*i != 0);
+            }
+            _ => {}
+        }
+    }
     output
 }
 
 fn interpret(code : &Vec<Instruction>) {
     let mut pc : usize = 0;
-    let mut mp : usize = 0;
-    let mut memory : Vec<u8> = vec![0; 1024];
+    let mut mp : u16 = 0;
+    let mut memory : Vec<u8> = vec![0; u16::max_value() as usize + 1];
     while pc < code.len() {
-        //println!("pc: {}, mp : {}", pc, mp);
-        //println!("Executing {:?}", code[pc]);
         match code[pc] {
             Instruction::PointerIncrement(i) => {
-                mp += i;
+                mp = mp.wrapping_add(i as u16);
                 pc += 1;
             }
             Instruction::PointerDecrement(i) => {
-                mp -= i;
+                mp = mp.wrapping_sub(i as u16);
                 pc += 1;
             }
             Instruction::Plus(i) => {
-                memory[mp] = memory[mp].wrapping_add(i);
+                memory[mp as usize] = memory[mp as usize].wrapping_add(i);
                 pc += 1;
             }
             Instruction::Minus(i) => {
-                memory[mp] = memory[mp].wrapping_sub(i);
+                memory[mp as usize] = memory[mp as usize].wrapping_sub(i);
                 pc += 1;
             }
             Instruction::Output(i) => {
-                for j in 0..i {
-                    let c: char = memory[mp].into();
+                for _ in 0..i {
+                    let c: char = memory[mp as usize].into();
                     print!("{}", c);
                 }
                 pc += 1;
             }
-            Instruction::Input(i) => {
+            Instruction::Input(_) => {
+                // TODO: Take some input
                 pc += 1;
             }
             Instruction::LoopBegin(i) => {
-                if memory[mp] == 0 {
+                if memory[mp as usize] == 0 {
                     pc = i;
                 } else {
                     pc += 1;
                 }
             }
             Instruction::LoopEnd(i) => {
-                if memory[mp] != 0 {
+                if memory[mp as usize] != 0 {
                     pc = i;
                 } else {
                     pc += 1;
