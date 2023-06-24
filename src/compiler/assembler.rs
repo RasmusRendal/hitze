4dc0ebfed72432bb2360cd0442133bf46c9d1f83
Rasmus Rendal <rasmus@rend.al>
2023-06-24 10:22:46 +0200

    Stop writing bytes directly

--- src/compiler/assembler.rs
+++ src/compiler/assembler.rs
@@ -46,8 +46,19 @@ pub unsafe fn allocate() -> Assembler {
     }
 }
 
+pub const RAX: u8 = 0b000;
+pub const RCX: u8 = 0b001;
+pub const RDX: u8 = 0b010;
+pub const RBX: u8 = 0b011;
+pub const RSP: u8 = 0b100;
+pub const RBP: u8 = 0b101;
+pub const RSI: u8 = 0b110;
+pub const RDI: u8 = 0b111;
+
+const REXW: u8 = 0x48;
+
 impl Assembler {
-    pub fn push_byte(&mut self, byte: u8) {
+    fn push_byte(&mut self, byte: u8) {
         unsafe {
             *(self.page.offset(self.cur_index) as *mut u8) = byte;
             self.cur_index += 1;
@@ -60,5 +71,40 @@ impl Assembler {
                 func: mem::transmute(self.page),
             }
         }
+    }
+
+    pub fn push(&mut self, reg: u8) {
+        self.push_byte(0x50 + reg);
+    }
+
+    pub fn pop(&mut self, reg: u8) {
+        self.push_byte(0x58 + reg);
+    }
+
+    pub fn ret(&mut self) {
+        self.push_byte(0xc3);
+    }
+
+    pub fn add_regmem8_imm8(&mut self, reg: u8, imm8: u8) {
+        self.push_byte(0x80);
+        self.push_byte(0b00_000_000 + (reg << 3));
+        self.push_byte(imm8);
+    }
+
+    pub fn sub_regmem8_imm8(&mut self, reg: u8, imm8: u8) {
+        self.push_byte(0x80);
+        self.push_byte(0b00_101_000 + reg);
+        self.push_byte(imm8);
+    }
+
+    pub fn add_al_imm8(&mut self, imm8: u8) {
+        self.push_byte(0x04);
+        self.push_byte(imm8);
+    }
+
+    pub fn mov_reg_reg(&mut self, dst: u8, src: u8) {
+        self.push_byte(REXW);
+        self.push_byte(0x89);
+        self.push_byte(0b11_000_000 + dst + (src << 3));
     }
 }
--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -2,56 +2,33 @@ use super::parser::Instruction;
 extern crate libc;
 mod assembler;
 use assembler::*;
-use std::mem;
 
 /* General notes
  * We store the pointer in %rax
  */
 
 pub fn compile(code: &Vec<Instruction>) -> Program {
-    //let contents = unsafe { allocate() };
     let mut assembler = unsafe { allocate() };
 
-    // Returning the current memory position does make sense after all
-    // Also, the addition ,instruction supports %rax directly
-    // PUSH RBP
-    assembler.push_byte(0x50 + 0x05);
-    // MOV RBP RSP
-    assembler.push_byte(0x48);
-    assembler.push_byte(0x89);
-    assembler.push_byte(0xE5);
-    // PUSH RDI
-    assembler.push_byte(0x50 + 0x07);
-    // POP RAX
-    assembler.push_byte(0x58 + 0x00);
+    assembler.push(RBP);
+    assembler.mov_reg_reg(RBP, RSP);
+
+    assembler.push(RDI);
+    assembler.pop(RAX);
 
     for instr in code {
         match *instr {
             Instruction::PointerIncrement(i) => {
-                // TODO: We are kind of assuming that i fits in an u8
-                // ADD id
-                assembler.push_byte(0x04);
-                // operand
-                assembler.push_byte(i as u8);
+                assembler.add_al_imm8(i as u8);
             }
             Instruction::PointerDecrement(i) => {
                 panic!("Unsupported instruction");
             }
             Instruction::Plus(i) => {
-                // 80 /0 ib
-                assembler.push_byte(0x80);
-                // ModRM
-                assembler.push_byte(0b00_000_000);
-                // Add with
-                assembler.push_byte(i as u8);
+                assembler.add_regmem8_imm8(RAX, i);
             }
             Instruction::Minus(i) => {
-                // 80 /0 ib
-                assembler.push_byte(0x80);
-                // ModRM
-                assembler.push_byte(0b00_101_000);
-                // Add with
-                assembler.push_byte(i as u8);
+                assembler.sub_regmem8_imm8(RAX, i);
             }
             Instruction::Output(i) => {
                 panic!("Unsupported instruction");
@@ -67,14 +44,9 @@ pub fn compile(code: &Vec<Instruction>) -> Program {
             }
         }
     }
-    //MOV RSP RBP
-    assembler.push_byte(0x48);
-    assembler.push_byte(0x89);
-    assembler.push_byte(0xec);
-    // POP RBP
-    assembler.push_byte(0x58 + 0x05);
-    // RET
-    assembler.push_byte(0xC3);
+    assembler.mov_reg_reg(RSP, RBP);
+    assembler.pop(RBP);
+    assembler.ret();
 
     assembler.create_program()
 }
